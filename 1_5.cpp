d16ea77d5818d701bbf670ae1bc0c6d29a7d064f
John Shumway <john.shumwayjr@gmail.com>
2017-02-10 23:18:20 -0500

    Update 1.5 with non-recursive solution

--- 1_5.cpp
+++ 1_5.cpp
@@ -30,22 +30,59 @@ int CountDiffs(
     CountDiffs(i1, end1, i2 + 1, end2, ndiff + 1));
 }
 
-bool IsOneAway(const std::string& s1, const std::string& s2) {
+bool IsOneAwayRecursive(const std::string& s1, const std::string& s2) {
+  // This is easily generalized to check for more than one difference
   int ndiff = CountDiffs(s1.begin(), s1.end(), s2.begin(), s2.end(), 0);
   return ndiff <= 1;
 }
 
+bool IsOneAway(const std::string& s1, const std::string& s2) {
+  if (abs(int(s1.size()) - int(s2.size())) > 1) {
+    return false;
+  }
+  bool used_edit = false;
+  auto iter1 = s1.begin();
+  auto iter2 = s2.begin();
+  for (; iter1 < s1.end() && iter2 < s2.end(); ++iter1, ++iter2) {
+    if (*iter1 != *iter2) {
+      // No match, need to use our edit
+      if (used_edit) {
+        return false;
+      }
+      used_edit = true;
+      // Chose to insert, delete, or replace based on string sizes
+      if (s1.size() < s2.size()) {
+        // Insert in s1
+        ++iter2;
+      }
+      else if (s1.size() > s2.size()) {
+        // Delete from  s1
+        ++iter1;
+      }
+      else {
+        // Replace in s1
+        ++iter1;
+        ++iter2;
+      }
+    }
+  }
+  return !used_edit || (iter1 == s1.end() && iter2 == s2.end());
+}
 
 int main() {
   std::string s1 = "pale", s2 = "ple";
+  std::cout << s1 << ", " << s2 << ": " << IsOneAwayRecursive(s1, s2) << std::endl;
   std::cout << s1 << ", " << s2 << ": " << IsOneAway(s1, s2) << std::endl;
 
   s1 = "pales", s2 = "pale";
+  std::cout << s1 << ", " << s2 << ": " << IsOneAwayRecursive(s1, s2) << std::endl;
   std::cout << s1 << ", " << s2 << ": " << IsOneAway(s1, s2) << std::endl;
 
   s1 = "pale", s2 = "bale";
+  std::cout << s1 << ", " << s2 << ": " << IsOneAwayRecursive(s1, s2) << std::endl;
   std::cout << s1 << ", " << s2 << ": " << IsOneAway(s1, s2) << std::endl;
 
   s1 = "pale", s2 = "bake";
+  std::cout << s1 << ", " << s2 << ": " << IsOneAwayRecursive(s1, s2) << std::endl;
   std::cout << s1 << ", " << s2 << ": " << IsOneAway(s1, s2) << std::endl;
 }
